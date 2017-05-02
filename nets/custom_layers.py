6a9847eda6cd3aef507bbd0fea735e6b09bcd29e
spades-k <294130021@qq.com>
2017-05-02 21:44:32 +0800

    test commit

---
+++ nets/custom_layers.py
@@ -0,0 +1,12 @@
+def pad2d(inputs, pad=(0, 0), mode = 'CONSTANT', data_format='NHWC', trainable= True, scope=None):
+    """2D Padding layer, adding a symmetric padding to H and W dimensions.
+
+    Aims to mimic padding in Caffe and MXnet, helping the port models to Tensorflow.
+    Tries to follow the naming convention of `tf.contrib.layers`.
+
+    Args:
+        inputs: 4D input Tensor;
+        pad: 2-Tuple with padding values for H and W dimensions;
+        mode: Padding mode. C.f. `tf.pad`
+        data_format: NHWC or HCHW data format.
+    """
